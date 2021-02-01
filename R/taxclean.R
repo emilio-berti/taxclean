f0a1ce747ae14e1054b30d0800a0dc371f507fba
emilio-berti <emilio.berti90@gmail.com>
2021-02-01 16:42:36 +0100

    GBIF table

--- R/taxclean.R
+++ R/taxclean.R
@@ -169,7 +169,7 @@ find_gbif <- function(sp, parallel = FALSE, n_cores = 6, alternative = FALSE) {
     }
     if (ans$data$matchType != "NONE") {
       if (nrow(ans$alternatives) == 0 | !alternative) {
-        return(ans$data$canonicalName)
+        return(gbif_table(ans$data[1, ]))
       } else {
         return(gbif_table(ans$alternatives[1, ]))
       }
