e4718b13a9bb8c9c6c8cbb76f75e8e1935587b5c
Emilio Berti <eb97ziwi@rstudio1.idiv.de>
2021-03-03 17:36:38 +0100

    Overlaps started

---
+++ R/databases_overlap.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(taxizedb)
+db_dow
+
+databases <- c("col", "gbif", "ncbi", "itis", "tpl", "wfo", "wikidata")
+
+# col --------
+db_download_col(overwrite = FALSE)
+conn <- DBI::dbConnect(RSQLite::SQLite(), path = src_col()$con@dbname)
+# NEED TO UNDERSTAND THE SQL TABLE
+
+# gbif ------
+db_download_gbif(overwrite = FALSE)
+conn <- DBI::dbConnect(RSQLite::SQLite(), path = src_gbif()$con@dbname)
+gbif <- sql_collect(src_gbif(), "SELECT DISTINCT * FROM gbif")$scientificName
+
+# itis ---------
+db_download_itis(overwrite = FALSE)
+conn <- DBI::dbConnect(RSQLite::SQLite(), path = src_itis()$con@dbname)
+itis <- sql_collect(src_itis(), "SELECT DISTINCT * FROM longnames")$completename
+
+# wfo -------
+db_download_wfo(overwrite = FALSE)
+conn <- DBI::dbConnect(RSQLite::SQLite(), path = src_wfo()$con@dbname)
+wfo <- sql_collect(src_wfo(), "SELECT DISTINCT scientificName FROM wfo")$scientificName
+
+# compute overlaps ------
+overlaps <- t(combn(databases, 2)) %>%
+  as_tibble() %>%
+  filter(V1 %in% c("itis", "wfo", "gbif"),
+         V2 %in% c("itis", "wfo", "gbif")) %>%
+  mutate(common_species_n = modify2(V1, V2, function(x, y) {
+    length(intersect(get(x), get(y)))
+  }))
+
+write_csv(overlaps, "overlaps.csv")
