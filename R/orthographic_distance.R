d101920e1a87c0c5020885272c6fcfdf5ca5d9fa
emilio-berti <emilio.berti90@gmail.com>
2021-03-10 16:01:26 +0100

    orthographic distance functions tidyed

--- R/orthographic_distance.R
+++ R/orthographic_distance.R
@@ -1,14 +1,23 @@
-#'   itle Compute Absolute and Relative Orthographic Distance
+#' @title Compute Absolute and Relative Orthographic Distance
+#' @description This function computes the absolute and relative orthographic
+#' distance as discussed in Meyer et al. (2016). Absolute orthographic distance
+#' is also known as Levenshtein distance, informally defined as the number of
+#' corrections necessary to reach a string from another. Relative orthographic
+#' distance is the absolute distance divided by the length of the string. Here,
+#' the relative distance is obtained as the Levenshtein distance divided by the
+#' average length of the strings.
 #'
-#'   This function computes the absolute and relative orthographic distance as
-#'   discussed in Meyer et al. (2016). Absolute orthographic distance is also
-#'   known as Levenshtein distance, informally defined as the number of
-#'   corrections necessary to reach a string from another. Relative orthographic
-#'   distance is the absolute distance divided by the length of the string.
-#'   Here, the relative distance is obtained as the Levenshtein distance divided
-#'   by the average length of the strings.
-
-orthographic_dist <- function(x, y, parallel = FALSE, n_cores = 4) {
+#' @param x first string or vector of strings.
+#' @param y second string or vector of strings.
+#' @param parallel TRUE/FALSE if to run in parallel.
+#' @param n_cores number of cores for parallel computation.
+#' @return a data.frame with absolute and relative orthographic distances.
+orthographic_dist <- function(
+  x,
+  y,
+  parallel = FALSE,
+  n_cores = 4
+) {
   if (length(x) != length(y)) {
     stop("Different number of strings in x and y")
   }
@@ -33,6 +42,36 @@ orthographic_dist <- function(x, y, parallel = FALSE, n_cores = 4) {
     levenshtein <- RecordLinkage::levenshteinDist(x, y)
     ans <- data.frame(abs.distance = levenshtein,
                       rel.distance = levenshtein / avg_length)
+    return(ans)
+  }
+}
+#' @title Check if Epithets match.
+#' @description Check if the epithets can considered to be matching each others
+#' according to Meyers et al. (2016). See also Jin & Yang (2020).
+#' @param dist data.frame with orthographic distances as returned from
+#' @param epithets (optional) data.frame with epithets. Is NULL, row numbers of
+#'   matching epithets are returned. \code{orthographic_distance()}.
+#' @return a data.frame with epithets, distance, and if can be considered
+#'   matching. If epithets = NULL, this returns only a vector with the row
+#'   numbers of matchin epithets.
+#' @examples
+#' x <- sapply(1:5000, function(x) paste(sample(letters[1:10], 5), collapse = ""))
+#' y <- sapply(1:5000, function(x) paste(sample(letters[1:10], 5), collapse = ""))
+#' dist <- orthographic_dist(x, y, parallel = TRUE)
+#' matches <- check_epithets_match(dist, data.frame(x, y))
+#' matches[matches$matching, ]
+check_epithets_match <- function(
+  dist,
+  epithets = NULL
+) {
+  ids <- which(dist$abs.distance < 4 & dist$rel.distance < 0.3)
+  if (is.null(epithets)) {
+    return(ids)
+  } else {
+    ans <- cbind(epithets, dist)
+    ans$matching <- sapply(seq_len(nrow(ans)), function(i) {
+      ifelse(i %in% ids, TRUE, FALSE)
+    })
     return(ans)
   }
 }
