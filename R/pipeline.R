df015f9f5218bb61d77b9a9d7ef17ab77aa0a377
emilio-berti <emilio.berti90@gmail.com>
2021-03-15 15:24:29 +0100

    fixed argument error

--- R/pipeline.R
+++ R/pipeline.R
@@ -1,34 +1,46 @@
-# function to append the correct function to the R script
-source("R/pipe_function.R")
-# packages chosen
-pkgs <- c("rotl", "rgnparser", "algaeClassify") #this would be the output of the choice made using the GUI.
-# rgnparser always first
-if ("rgnparser" %in% pkgs) {
-  pkgs <- append("rgnparser", pkgs)
-  pkgs <- pkgs[-which(pkgs == "rgnparser")[2]]
+#' @title Write R script Pipeline
+#' @description Writes to a file the R code to reproduce a pipeline..
+#' @param pkgs packages to call sequentially.
+#' @param pipeline_script file where to write the R code.
+write_pipeline <- function(
+  pkgs = c("rotl", "rgnparser", "algaeClassify"), #NULL,
+  pipeline_script = "pipelines/template.R" #NULL
+) {
+  if (is.null(pkgs)) {
+    stop("Nothing to write")
+  }
+  if (is.null(pipeline_script)) {
+    stop("No file specified where to write the pipeline")
+  }
+  # function to append the correct function to the R script
+  source("R/pipe_function.R")
+  # packages chosen
+  # rgnparser always first
+  if ("rgnparser" %in% pkgs) {
+    pkgs <- append("rgnparser", pkgs)
+    pkgs <- pkgs[-which(pkgs == "rgnparser")[2]]
+  }
+  # writing the script -----------
+  write(paste0('source("R/', pkgs, '.R")'), pipeline_script, append = FALSE)
+  write('# load here you dataset', pipeline_script, append = TRUE)
+  write('d <- read.csv("datasets.csv")[, 1] #rename this', pipeline_script, append = TRUE)
+  # output from rgnparser is not standard with other functions.
+  # this also writes the call of the first tc_function, which takes the dataset as input.
+  if (pkgs[1] == "rgnparser") {
+    write('message("RGNparser")', pipeline_script, append = TRUE)
+    write('ans <- tc_rgnparser(d)', pipeline_script, append = TRUE)
+    write(paste0('message("', pkgs[2], '")'), pipeline_script, append = TRUE)
+    write(paste0('ans <- ', pipe_function(pkgs[2], after_gnr = TRUE)), pipeline_script, append = TRUE)
+    pkgs <- setdiff(pkgs, pkgs[1])
+  } else {
+    write(paste0('message("', pkgs[1], '")'), pipeline_script, append = TRUE)
+    write(paste0('ans <- ', pipe_function(pkgs[1], first = TRUE)), pipeline_script, append = TRUE)
+  }
+  # iterate over all pacakges, except first one (rgnparser is already removed, if
+  # present originally).
+  for (x in setdiff(pkgs, pkgs[1])) {
+    write(paste0('message("', x, '")'), pipeline_script, append = TRUE)
+    write(paste0('ans <- ', pipe_function(x)), pipeline_script, append = TRUE)
+  }
+  write('write.csv(ans, "pipelines/pipeline_result.csv")', pipeline_script, append = TRUE)
 }
-# file to write the R script
-file <- "pipelines/template.R"
-# writing the script -----------
-write(paste0('source("R/', pkgs, '.R")'), file, append = FALSE)
-write('# load here you dataset', file, append = TRUE)
-write('d <- read.csv("datasets.csv")[, 1] #rename this', file, append = TRUE)
-# output from rgnparser is not standard with other functions.
-# this also writes the call of the first tc_function, which takes the dataset as input.
-if (pkgs[1] == "rgnparser") {
-  write('message("RGNparser")', file, append = TRUE)
-  write('ans <- tc_rgnparser(d)', file, append = TRUE)
-  write(paste0('message("', pkgs[2], '")'), file, append = TRUE)
-  write(paste0('ans <- ', pipe_function(pkgs[2], after_gnr = TRUE)), file, append = TRUE)
-  pkgs <- setdiff(pkgs, pkgs[1])
-} else {
-  write(paste0('message("', pkgs[1], '")'), file, append = TRUE)
-  write(paste0('ans <- ', pipe_function(pkgs[1], first = TRUE)), file, append = TRUE)
-}
-# iterate over all pacakges, except first one (rgnparser is already removed, if
-# present originally).
-for (x in setdiff(pkgs, pkgs[1])) {
-  write(paste0('message("', x, '")'), file, append = TRUE)
-  write(paste0('ans <- ', pipe_function(x)), file, append = TRUE)
-}
-write('write.csv(ans, "pipelines/pipeline_result.csv")', file, append = TRUE)
